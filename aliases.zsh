161835972517b7844875b6734b9a7214df41d833
Baptiste Arnaud <baptiste.arnaud95@gmail.com>
2022-09-10 10:17:11 +0200

    :wrench: Misc changes

--- aliases.zsh
+++ aliases.zsh
@@ -2,20 +2,24 @@
 alias gp="git push"
 alias gpf="git push --force-with-lease"
 alias gpf!="git push --force"
+alias gf="git fetch --all"
 alias gpl="git pull"
 alias gr="git rebase"
 alias gc="git add . && cz"
-alias grm="gr origin/main"
+alias grm="gf && gr origin/main"
 alias gcr="gc && grm"
 alias grp="grm && gpf"
 alias gcrp="gc && grp"
+alias gcrpb="gcr && git push -u origin HEAD"
 alias gca="git add . && git commit --amend --no-edit"
 alias gcarp="gca && grp"
-alias gcom="gco main && grp"
+alias gcom="gco main && grm && gpl"
 alias gsta="git add . && git stash"
 alias gstap="git stash pop"
 alias gco="git checkout"
 alias gbd="git branch -D"
+alias gs="git status"
+alias gl="git log --graph --oneline --decorate"
 
 gb() { git branch $1 && gco $1 }
 gcor() { gco $1 && grm }
@@ -31,3 +35,9 @@ alias cma="chezmoi apply -v"
 alias cmc="chezmoi cd"
 
 alias ls="exa -l"
+
+# ni
+alias nrs="nr start"
+alias nrb="nr build"
+alias nrd="nr dev"
+alias nrdm="nr dev:mocking"
