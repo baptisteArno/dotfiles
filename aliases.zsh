e44577a4a43e9466d79ddeb84169a083d324ca0a
Baptiste Arnaud <baptiste.arnaud95@gmail.com>
2023-03-11 09:26:08 +0100

    :fire: Remove kitty config
    Also update git aliases

--- aliases.zsh
+++ aliases.zsh
@@ -3,27 +3,33 @@ alias gp="git push"
 alias gpf="git push --force-with-lease"
 alias gpf!="git push --force"
 alias gf="git fetch --all"
-alias gpl="git pull"
+alias gpl="git pull --rebase"
 alias gr="git rebase"
-alias gc="git add . && cz"
+alias grc="git rebase --continue"
+alias gra="git rebase --abort"
+alias ga="git add -A"
+alias gc="cz"
 alias grm="gf && gr origin/main"
 alias gcr="gc && grm"
 alias grp="grm && gpf"
+alias gcp="gc && gp"
 alias gcrp="gc && grp"
 alias gcrpb="gcr && git push -u origin HEAD"
 alias gca="git add . && git commit --amend --no-edit"
+alias gcap="gca && gpf"
 alias gcarp="gca && grp"
 alias gcom="gco main && grm && gpl"
-alias gsta="git add . && git stash"
 alias gstap="git stash pop"
 alias gco="git checkout"
 alias gbd="git branch -D"
 alias gs="git status"
-alias gl="git log --graph --full-history --all --color --pretty=format:'%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s'"
+alias gl="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --branches --remotes --tags"
+alias gd="git diff"
 
 gb() { git branch $1 && gco $1 }
 gcor() { gco $1 && grm }
 gt() {git tag $1 && git push origin $1}
+gsta() {git add . && git stash push --include-untracked -m "$1"}
 
 # yalc
 alias yalcp="nr build && yalc publish --push --sig"
